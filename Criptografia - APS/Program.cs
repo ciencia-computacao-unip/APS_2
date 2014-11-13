98370b3720c959c5141a7efb53b2d290ad466249
Victor Perin <victor_perin@nerdhitech.com.br>
2014-11-13 16:16:17 -0300

    Comentários no código da hash e trocando as hashs

--- Criptografia - APS/Program.cs
+++ Criptografia - APS/Program.cs
@@ -38,24 +38,25 @@ namespace Criptografia___APS
 
                 int tamanho_da_hash = 64; //tamanho da hash a ser criada
                 char[] texto_tamanho_fixo_array = new char[tamanho_da_hash]; //cria um novo array que vai deixar o texto em um tamanho fixo
-                for (int x = 0; x < tamanho_da_hash; x++)
+                for (int x = 0; x < tamanho_da_hash; x++) 
                 {
-                    int y = x % (texto_char.Length);
-                    int modificador_x = x % 255;
-                    int modificador_y = y % 255;
+                    int y = x % (texto_char.Length); //y serve para copiar os characteres se o texto for menor do que o tamanho da hash
+                    int modificador_x = x % 255; // modificador x é relacionado ao tamanho da hash (ele é usado para fazer contas com o charactere)
+                    int modificador_y = y % 255; // modificador x é relacionado ao tamanho do testo (ele é usado para fazer contas com o charactere)
                     
-                    int numero_char = (int)(texto_char[y]), numero_anterior;
-                    if (y > 0) { numero_anterior = (int)(texto_char[y - 1]); } else { numero_anterior = 1; }
-                    int numero_novo_char = (int)(((numero_anterior + 32) + modificador_x * numero_char * (modificador_x * 0.3 + modificador_y)) % 255);
-                    while (numero_novo_char < 33 || numero_novo_char == 127 || numero_novo_char == 0) { numero_novo_char += ((numero_anterior+numero_novo_char + numero_novo_char * (x + 1)) % 255); }
-                    char novo_char = (char)(numero_novo_char);
-                    texto_tamanho_fixo_array[x] = novo_char;
+                    int numero_char = (int)(texto_char[y]), numero_anterior; // numero_char converte o charactere em número
+                    if (y > 0) { numero_anterior = (int)(texto_char[y - 1]); } else { numero_anterior = 1; } //numero_anterior é o charactere anterior também em número
+                    int numero_novo_char = (int)(((numero_anterior + 32) + modificador_x * numero_char * (modificador_x * 0.3 + modificador_y)) % 255); //a maioria das operações de criptografia são feitas nesse passo
+                    while (numero_novo_char < 33 || numero_novo_char == 127 || numero_novo_char == 0) { numero_novo_char += ((numero_anterior+numero_novo_char + numero_novo_char * (x + 1)) % 255); } //se o charactere for um charactere como backspace, enter, etc, o programa faz outras operações até o mesmo não poder ser utilizado
+                    char novo_char = (char)(numero_novo_char); //transforma o número de volta em caractere
+                    texto_tamanho_fixo_array[x] = novo_char; //insere o caractere no array
                 }
-                string texto_tamanho_fixo = string.Join("", texto_tamanho_fixo_array);
-                return texto_tamanho_fixo;
+                string texto_tamanho_fixo = string.Join("", texto_tamanho_fixo_array); //junta os arrays na string
+                return texto_tamanho_fixo; //retorna o texto hasheado
             }
-            return "Texto sem senha!Texto sem senha!Texto sem senha!Texto sem senha!";
+            return "Texto sem senha!Texto sem senha!Texto sem senha!Texto sem senha!"; //retorna esse testo, caso a string seja vazia
         }
+
         public static string Criptografar(string text, string cryptoKey)
         {
             try
@@ -168,13 +169,9 @@ namespace Criptografia___APS
             string hora = DateTime.Now.ToString("ddMMyyhmmss");
             string segundos = DateTime.Now.ToString("ss");
             string senha_descriptografada = senha + hora;
+            string senha_criptografada = Criptografia.GerarHash(senha_descriptografada);
 
-            SHA256Managed crypt = new SHA256Managed();
-            string hash = String.Empty, senha_sha256 = String.Empty;
-            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(senha_descriptografada), 0, Encoding.ASCII.GetByteCount(senha_descriptografada));
-            foreach (byte bit in crypto) { senha_sha256 += bit.ToString("x2"); }
-            string senha_32bytes_md5 = System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(senha_sha256.Substring(0, 32)));
-            string mensagem_criptografada = Criptografia.Criptografar(texto, senha_32bytes_md5);
+            string mensagem_criptografada = Criptografia.Criptografar(texto, senha_criptografada);
             StreamWriter writer = new StreamWriter(local_arquivo, true);
             using (writer)
             {
@@ -224,13 +221,9 @@ namespace Criptografia___APS
 
                     string senha_descriptografada = senha + horario_mensagem;
 
-                    SHA256Managed crypt = new SHA256Managed();
-                    string hash = String.Empty, senha_sha256 = String.Empty;
-                    byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(senha_descriptografada), 0, Encoding.ASCII.GetByteCount(senha_descriptografada));
-                    foreach (byte bit in crypto) { senha_sha256 += bit.ToString("x2"); }
-                    string senha_32bytes_md5 = System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(senha_sha256.Substring(0, 32)));
+                    string senha_criptografada = Criptografia.GerarHash(senha_descriptografada);
 
-                    string mensagem_descriptografada = Criptografia.Descriptografar(texto_criptografado, senha_32bytes_md5);
+                    string mensagem_descriptografada = Criptografia.Descriptografar(texto_criptografado, senha_descriptografada);
 
                     //Console.Write(DecryptMessage(texto_criptografado, senha_criptografada_diminuida) + "\n");
                     Console.Write(mensagem_descriptografada + "\n");
